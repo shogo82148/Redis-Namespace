5e1933645e0b4af26dea5279e3e596f713f7baeb
Ichinose Shogo <shogo82148@gmail.com>
2013-09-08 13:33:01 +0900

    initial commit

---
+++ lib/Redis/Namespace.pm
@@ -0,0 +1,32 @@
+package Redis::Namespace;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Redis::Namespace -
+
+=head1 SYNOPSIS
+
+  use Redis::Namespace;
+
+=head1 DESCRIPTION
+
+Redis::Namespace is
+
+=head1 AUTHOR
+
+Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
